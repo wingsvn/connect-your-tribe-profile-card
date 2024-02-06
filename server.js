cb0dd47253d42eec9882d2e8e194881a47da7cbe
wingsvn <wing-san.lee@live.nl>
2024-02-06 12:02:30 +0100

    V1 old profile card

--- server.js
+++ server.js
@@ -5,14 +5,15 @@ import express from 'express'
 import fetchJson from './helpers/fetch-json.js'
 
 // Haal data op uit de FDND API, ga pas verder als de data gedownload is
-const data = await fetchJson('https://fdnd.directus.app/items/person/65')
-// console.log(data); // uncomment om de opgehaalde data te checken
+const data = await fetchJson('https://fdnd.directus.app/items/person/10')
+console.log(data.data.name); // uncomment om de opgehaalde data te checken
 
 // Maak een nieuwe express app aan
 const app = express()
 
 // Stel ejs in als template engine
 app.set('view engine', 'ejs')
+
 // Stel de map met ejs templates in
 app.set('views', './views')
 
